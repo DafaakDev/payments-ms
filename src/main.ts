b402c537c431b90452df332dfd1ed86cdbae1e75
Israel <israel.pe@outlook.es>
2025-02-25 16:50:17 -0500

    feat: added global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config/envs';
 
 async function bootstrap() {
   const logger = new Logger('PAYMENTS-MS');
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(envs.port);
   logger.log(`PAYMENTS MS running on port ${envs.port}`);
 }
